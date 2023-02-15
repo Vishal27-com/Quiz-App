ea7b8414ce3c5adca6cb3c27530098cead9b9652
Vishal27-com <vishalgupta962148@gmail.com>
2023-02-15 17:20:57 +0530

    Logo

---
+++ Frontend/src/Components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from "./Logo.module.css";
+const Logo = () => {
+  return (
+    <div id={style.logo}>
+        <p>Quizester</p>    
+    </div>
+  )
+}
+
+export default Logo
