54a109cdc2244c3a861ad22e733864119a131c2f
Vishal27-com <vishalgupta962148@gmail.com>
2023-02-15 17:22:13 +0530

    api updated

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL="http://localhost:8080"
+const BASE_URL="http://localhost:8080" 
 export const signupApi=(creds)=>{
     return axios.post(`${BASE_URL}/auth/signup`,creds);
 }
