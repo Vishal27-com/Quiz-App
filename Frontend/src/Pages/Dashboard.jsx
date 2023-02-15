a493d2d6450495998111a28409a15a39cf99458e
Vishal27-com <vishalgupta962148@gmail.com>
2023-02-15 17:19:48 +0530

    Dashboard

---
+++ Frontend/src/Pages/Dashboard.jsx
@@ -0,0 +1,24 @@
+import { SimpleGrid } from '@chakra-ui/react'
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+import Topicard from '../Components/Topic Card/Topicard'
+import { topic } from '../Components/Topic Card/topicData'
+
+const Dashboard = () => {
+  return (
+    <div>
+       <Navbar />
+       <SimpleGrid w='90%' m='40px auto' columns={3} spacing={10} >
+       {
+        topic.map((item)=>
+        <Topicard 
+        image={item.image}
+        topic={item.topic}
+        />)
+       }
+       </SimpleGrid> 
+    </div>
+  )
+}
+
+export default Dashboard
