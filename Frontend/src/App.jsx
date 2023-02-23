b664e356c820e80953e7ec0d50d03e67e5ff630f
Vishal27-com <vishalgupta962148@gmail.com>
2023-02-23 18:33:48 +0530

    Package Updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import "./App.css"
-import AllRoutes from './Components/AllRoutes'
+import React from "react";
+import "./App.css";
+import AllRoutes from "./Components/Routes/AllRoutes";
 const App = () => {
+ 
   return (
-    <div id='App'>
+    <div id="App">
       <AllRoutes />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {ChakraProvider} from "@chakra-ui/react"
-import { BrowserRouter } from 'react-router-dom'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./Context/AuthContext";
+import { store } from "./Redux/store";
+import {Provider} from "react-redux"
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <ChakraProvider>
-    <App />
-    </ChakraProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </BrowserRouter>
+      </AuthContextProvider>
+    </Provider>
+  </React.StrictMode>
+);
