872659d2c0442d4681a9917934a800061f63979a
Vishal27-com <vishalgupta962148@gmail.com>
2023-02-23 18:30:12 +0530

    quiz api added

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,8 +1,20 @@
 import axios from "axios";
-const BASE_URL="http://localhost:8080" 
+const BASE_URL="https://quizester.onrender.com/" 
 export const signupApi=(creds)=>{
     return axios.post(`${BASE_URL}/auth/signup`,creds);
 }
 export const loginApi=(creds)=>{
     return axios.post(`${BASE_URL}/auth/login`,creds);
+}
+export const getUserApi=(id)=>{
+    return axios.get(`${BASE_URL}/auth/${id}`);
+}
+export const updateUserApi=(id,data)=>{
+    return axios.patch(`${BASE_URL}/auth/${id}`,data);
+}
+export const postQuestionApi=(data)=>{
+    return axios.post(`${BASE_URL}/quiz`,data);
+}
+export const getQuestionApi=(type,level,limit)=>{
+    return axios.get(`${BASE_URL}/quiz/?type=${type}&level=${level}&limit=${limit}`);
 }
