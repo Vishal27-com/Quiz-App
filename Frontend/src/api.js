ca7d0765bc4b85cf3494d0eeb7d59df33c1c57d8
Vishal27-com <vishalgupta962148@gmail.com>
2023-03-01 19:59:07 +0530

    api updated

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -9,14 +9,32 @@ export const loginApi=(creds)=>{
 export const getUserApi=(id)=>{
     return axios.get(`${BASE_URL}/auth/${id}`);
 }
+export const getAllUserApi=(filter="both",sort="asc",page=1,limit=10)=>{
+    return axios.get(`${BASE_URL}/auth/?filterby=${filter}&sortby=${sort}&page=${page}&limit=${limit}`);
+}
 export const updateUserApi=(id,data)=>{
     return axios.patch(`${BASE_URL}/auth/${id}`,data);
+}
+export const deleteUserApi=(id)=>{
+    return axios.delete(`${BASE_URL}/auth/${id}`);
 }
 export const postQuestionApi=(data)=>{
     return axios.post(`${BASE_URL}/quiz`,data);
 }
+export const deleteQuestionApi=(id)=>{
+    return axios.delete(`${BASE_URL}/quiz/${id}`);
+}
+export const updateQuestionApi=(id,data)=>{
+    return axios.patch(`${BASE_URL}/quiz/${id}`,data);
+}
 export const getQuestionApi=(type,level,limit)=>{
     return axios.get(`${BASE_URL}/quiz/?type=${type}&level=${level}&limit=${limit}`);
+}
+export const getQuestionByIdApi=(id)=>{
+    return axios.get(`${BASE_URL}/quiz/question/${id}`);
+}
+export const getAllQuestionApi=(type,level,sort,page,limit)=>{
+    return axios.get(`${BASE_URL}/quiz/all/?type=${type}&level=${level}&sortby=${sort}&page=${page}&limit=${limit}`);
 }
 export const postResultApi=(data)=>{
     return axios.post(`${BASE_URL}/result`,data);
