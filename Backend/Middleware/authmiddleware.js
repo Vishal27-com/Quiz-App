e1f3c4e3508fc0d02e7c380013cf5b634ceb56de
Vishal27-com <vishalgupta962148@gmail.com>
2023-03-02 14:49:27 +0530

    error removed

--- Backend/Middleware/authmiddleware.js
+++ Backend/Middleware/authmiddleware.js
@@ -4,7 +4,7 @@ const authmiddleware = (req, res, next) => {
   if (!token) {
     return res.status(401).send({message:"You are not authenticated" ,error:true});
   }
-  jwt.verify(token, process.env.JWT, (err, user) => {
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) return res.status(403).send({message:"Token is not valid" ,error:true});
     next();
   });
