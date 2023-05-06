167be34f5c83c2ff494f328dd54765bbc7510395
Vishal27-com <vishalgupta962148@gmail.com>
2023-05-06 15:38:03 +0530

    db connect updated

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,7 @@
 const mongoose =require("mongoose");
-const dbConnect=async()=>{
+const dbConnect=()=>{
     try {
-        await mongoose.connect(process.env.DB_URI);
+        mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
         console.log("Database Connected");
     } catch (error) {
         console.log(error.message);
